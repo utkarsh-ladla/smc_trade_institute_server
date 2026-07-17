ac2d48dc90aa9727588a0c6de09269fc63bc1348
utkarsh-ladla <utkarshladla@gmail.com>
2026-07-17 15:03:02 +0530

    fix: update CORS allowed origins to include smclassesranjhi.in and remove inspiredwingspublication.co

--- app/main.py
+++ app/main.py
@@ -34,8 +34,8 @@ app.add_middleware(
         "http://127.0.0.1:3000",
         "http://localhost:3001",
         "http://127.0.0.1:3001",
-        "http://inspiredwingspublication.co",
-        "https://smc-trade-institute.vercel.app"
+        "https://smc-trade-institute.vercel.app",
+        "https://smclassesranjhi.in/"
     ],
     allow_credentials=True,
     allow_methods=["*"],
